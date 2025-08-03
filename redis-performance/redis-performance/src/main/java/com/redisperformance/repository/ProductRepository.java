3711f093baa8a8ca292952bdacf1200278143151
Ridho Suhaebi Arrowi <ridhorezi1212@gmail.com>
2025-08-03 11:30:51 +0700

    Finish spring reactive redis masterclass

---
+++ redis-performance/redis-performance/src/main/java/com/redisperformance/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.redisperformance.repository;
+
+import com.redisperformance.entity.Product;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends ReactiveCrudRepository<Product, Integer> {
+}
---
+++ redis-performance/redis-performance/src/main/java/com/redisperformance/service/ProductServiceV1.java
@@ -0,0 +1,24 @@
+package com.redisperformance.service;
+
+import com.redisperformance.entity.Product;
+import com.redisperformance.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class ProductServiceV1 {
+
+	@Autowired
+	private ProductRepository repository;
+
+	public Mono<Product> getProduct(int id) {
+		return this.repository.findById(id);
+	}
+
+	public Mono<Product> updateProduct(int id, Mono<Product> productMono) {
+		return this.repository.findById(id).flatMap(p -> productMono.doOnNext(pr -> pr.setId(id)))
+				.flatMap(this.repository::save);
+	}
+
+}
